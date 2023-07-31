b2f09a11074bda025c8c03f028b9db428d7beca7
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-31 14:58:41 +0700

    Add entity model class.

---
+++ app/src/main/java/com/yumtaufikhidayat/jetnotes/featurenotes/domain/model/Notes.kt
@@ -0,0 +1,27 @@
+package com.yumtaufikhidayat.jetnotes.featurenotes.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.yumtaufikhidayat.jetnotes.ui.theme.BabyBlue
+import com.yumtaufikhidayat.jetnotes.ui.theme.LightGreen
+import com.yumtaufikhidayat.jetnotes.ui.theme.RedOrange
+import com.yumtaufikhidayat.jetnotes.ui.theme.RedPink
+import com.yumtaufikhidayat.jetnotes.ui.theme.Violet
+
+@Entity
+data class Notes(
+    @PrimaryKey
+    val id: Int? = null,
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+) {
+    val noteColors = listOf(
+        RedOrange,
+        LightGreen,
+        Violet,
+        BabyBlue,
+        RedPink
+    )
+}
