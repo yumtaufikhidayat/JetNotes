23fabc3b501a88e2a64b269a31758cefeb896fe3
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-31 15:58:13 +0700

    Add add notes use case class to wrapper use case class.

--- app/src/main/java/com/yumtaufikhidayat/jetnotes/featurenotes/domain/usecase/NotesWrapperUseCase.kt
+++ app/src/main/java/com/yumtaufikhidayat/jetnotes/featurenotes/domain/usecase/NotesWrapperUseCase.kt
@@ -2,5 +2,6 @@ package com.yumtaufikhidayat.jetnotes.featurenotes.domain.usecase
 
 data class NotesWrapperUseCase(
     val getNotesUseCase: GetNotesUseCase,
-    val deleteNotesUseCase: DeleteNotesUseCase
+    val deleteNotesUseCase: DeleteNotesUseCase,
+    val addNotesUseCase: AddNotesUseCase
 )
