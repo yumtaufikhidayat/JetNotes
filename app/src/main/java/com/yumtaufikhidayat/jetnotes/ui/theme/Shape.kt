45987f8b2a11a3f82484b4df7050ee12e43ff4c4
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-02 21:15:06 +0700

    Update imports of shape, theme, and type.

--- app/src/main/java/com/yumtaufikhidayat/jetnotes/ui/theme/Shape.kt
+++ app/src/main/java/com/yumtaufikhidayat/jetnotes/ui/theme/Shape.kt
@@ -1,7 +1,7 @@
 package com.yumtaufikhidayat.jetnotes.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Shapes
+import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val shape = Shapes(
