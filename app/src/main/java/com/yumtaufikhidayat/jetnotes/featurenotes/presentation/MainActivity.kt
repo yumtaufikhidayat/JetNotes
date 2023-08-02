e6cef264bbd924a9af339870e44a63bcd5f7544a
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-02 21:18:50 +0700

    Update MainActivity class.

--- app/src/main/java/com/yumtaufikhidayat/jetnotes/featurenotes/presentation/MainActivity.kt
+++ app/src/main/java/com/yumtaufikhidayat/jetnotes/featurenotes/presentation/MainActivity.kt
@@ -3,35 +3,62 @@ package com.yumtaufikhidayat.jetnotes.featurenotes.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.yumtaufikhidayat.jetnotes.featurenotes.presentation.addeditnotes.AddEditNotesScreen
+import com.yumtaufikhidayat.jetnotes.featurenotes.presentation.notes.NotesScreen
+import com.yumtaufikhidayat.jetnotes.featurenotes.presentation.utils.Screen
 import com.yumtaufikhidayat.jetnotes.ui.theme.JetNotesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetNotesTheme {
-
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NotesScreen.route
+                    ) {
+                        composable(route = Screen.NotesScreen.route) {
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNotesScreen.route +
+                                    "?notesId={notesId}&notesColor={notesColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "notesId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "notesColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                }
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNotesScreen(navController = navController, notesColor = color)
+                        }
+                    }
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetNotesTheme {
-        Greeting("Android")
     }
 }
