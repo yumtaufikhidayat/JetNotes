3890dfb0372fa3863e34cdffee82010a9396b238
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-03 09:40:06 +0700

    Capitalize name variable.

--- app/src/main/java/com/yumtaufikhidayat/jetnotes/ui/theme/Shape.kt
+++ app/src/main/java/com/yumtaufikhidayat/jetnotes/ui/theme/Shape.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
-val shape = Shapes(
+val Shape = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(0.dp)
