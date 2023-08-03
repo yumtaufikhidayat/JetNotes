723ad10a4f32ff24a8e2e6ecf0b0ae47c512f219
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-03 09:39:38 +0700

    Add light theme for device theme compatibility.

--- app/src/main/java/com/yumtaufikhidayat/jetnotes/ui/theme/Theme.kt
+++ app/src/main/java/com/yumtaufikhidayat/jetnotes/ui/theme/Theme.kt
@@ -1,12 +1,18 @@
 package com.yumtaufikhidayat.jetnotes.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val darkColorPalette = darkColors(
+private val DarkColorPalette = darkColors(
     primary = Color.White,
     background = DarkGray,
     onBackground = Color.White,
@@ -14,17 +20,33 @@ private val darkColorPalette = darkColors(
     onSurface = DarkGray
 )
 
+private val LightColorScheme = lightColors(
+    primary = Purple40,
+    secondary = PurpleGrey40
+)
+
 @Composable
 fun JetNotesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val colorScheme = when {
+        darkTheme -> DarkColorPalette
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
     MaterialTheme(
-        colors = darkColorPalette,
+        colors = colorScheme,
         typography = Typography,
-        shapes = shape,
+        shapes = Shape,
         content = content
     )
 }
