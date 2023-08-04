6c84b1e784780652128aff1396e6309b21389759
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-04 07:47:44 +0700

    Move class to package component.

--- app/src/main/java/com/yumtaufikhidayat/jetnotes/featurenotes/presentation/addeditnotes/components/TransparentTextHintField.kt
+++ app/src/main/java/com/yumtaufikhidayat/jetnotes/featurenotes/presentation/addeditnotes/components/TransparentTextHintField.kt
@@ -1,4 +1,4 @@
-package com.yumtaufikhidayat.jetnotes.featurenotes.presentation.addeditnotes
+package com.yumtaufikhidayat.jetnotes.featurenotes.presentation.addeditnotes.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
